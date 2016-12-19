d8c2232963f251133e45817619e2c875a21a49c7
Kenneth Kang <email": null>
2016-12-19 16:30:37 -0600

    add gps location to message

--- rails_server/app/controllers/messages_controller.rb
+++ rails_server/app/controllers/messages_controller.rb
@@ -19,7 +19,7 @@ skip_before_action :verify_authenticity_token
     @sender = User.find(session[:user])
     @conversation
     @message = Message.new(sender_id:@sender.id,receiver_id:@receiver.id,body:params[:message][:body])
-    @message.location = params[:location] # this is optional
+    @message.location = params[:message][:location] # this is optional; may need to specify or nil/string
     if @message.save
       p "%%%%%%%SUCCESSFUL MESSAGE CREATION%%%%%%"
     else
