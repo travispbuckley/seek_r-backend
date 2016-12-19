28f952fd9add359bcaf90f88d6a1887c462a99a5
Kenneth Kang <email": null>
2016-12-19 16:16:24 -0600

    adding migrate

--- rails_server/db/schema.rb
+++ rails_server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161217215231) do
+ActiveRecord::Schema.define(version: 20161217214757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,14 +28,6 @@ ActiveRecord::Schema.define(version: 20161217215231) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-  end
-
-  create_table "users_messages", force: :cascade do |t|
-    t.integer  "sender_id",   null: false
-    t.integer  "reciever_id", null: false
-    t.integer  "message_id",  null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
