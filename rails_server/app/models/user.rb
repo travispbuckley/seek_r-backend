161b22f2c5b475a6ae796c66e3ab4fa7d8d823f3
Kenneth Kang <kenneth.g.kang@vanderbilt.edu>
2016-12-21 10:14:05 -0600

    simply removed external folder

--- rails_server/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-
-  validates :username, :password_digest, presence: true, uniqueness: true
-  has_many  :sent_messages, foreign_key: :sender_id, class_name: "Message"
-  has_many  :received_messages, foreign_key: :receiver_id, class_name: "Message"
-  has_secure_password
-end
--- rails_server/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :users
-   resources :sessions
-   resources :messages
-
-   post '/sessions/check' => 'sessions#check'
-
-end
