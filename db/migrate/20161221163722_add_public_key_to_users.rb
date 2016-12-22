6dd66e64f1395863edf145a8f3c2004df41d49f1
Kenneth Kang <kenneth.g.kang@vanderbilt.edu>
2016-12-22 11:31:49 -0600

    update string length

--- db/migrate/20161221163722_add_public_key_to_users.rb
+++ db/migrate/20161221163722_add_public_key_to_users.rb
@@ -1,6 +1,6 @@
 class AddPublicKeyToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :public_key_n, :string
+    add_column :users, :public_key_n, :string, :limit => 500
     add_column :users, :public_key_e, :string
     add_column :messages, :your_message, :string
   end
